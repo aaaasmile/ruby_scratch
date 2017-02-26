01f010cb9e2568a9b2ac2d04f344dfb1fc4b0c7e
aaaasmile <testhaus@gmx.at>
2017-02-26 21:54:15 +0100

    add all urls

--- finanz_net/get_quote.rb
+++ finanz_net/get_quote.rb
@@ -30,6 +30,7 @@ class EtfQuoteFinanzNet
       if element_with_quote
         #p element.inner_html
         i = 0
+        # qui una linea separata da td di questo tipo: LU0292106167 XetraETF 18,39 24.02./17 36 -0,05/-0,27%
         element.search('td').each do |field|
           if i == 0 || i == 1 || i == 4 || i == 5
             item = field.inner_html.gsub(' ','')
@@ -47,7 +48,8 @@ class EtfQuoteFinanzNet
         element_with_quote = true # mark the next item  
       end
     end
-    p data_found
+    #p data_found
+    data_found << url
     @picked_quote[data_found[0]] = data_found
   end
   
@@ -56,10 +58,11 @@ class EtfQuoteFinanzNet
       @picked_quote.each do |k,v|
         file.write(v.join(';'))
         file.write("\n")
+        puts "ISIN: #{v[0]}, quote: #{v[2]}"
       end
     end
     puts "File written #{fname}"
-    p @picked_quote
+    #p @picked_quote
   end
   
 end
@@ -71,9 +74,34 @@ if $0 == __FILE__
   #picker.pick_from_complete_url(url)
   
   urls = []
-  
-  #LU0292106167
+  # 1 LU0290357929  db x-trackers II iBoxx Global Infl.-linked UCITS ETF 1C
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-10904993&menuId=1&pathName=DB%20X-TR.II-IB.GL.IN.-L.1C'
+  # 2 LU0290355717  db x-trackers II iBoxx Sov. Eurozone UCITS ETF 1C
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-10904984&menuId=1&pathName=DBXTR.II-EUROZ.G.B.(DR)1C'
+  # 3 LU0292106167  db x-trackers DBLCI - OY Balanced UCITS ETF 1C
   urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-11057070&menuId=1&pathName=DB%20X-TR.DBLCI-OY%20BAL.%201C'
+  # 4 LU0489337690 db x-trackers FTSE EPRA/NAREIT Dev. Europe Real Estate
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-23270949&menuId=1&pathName=DB%20X-T.FTSE%20E/N%20DE%20RE%201C'
+  # 5 IE00B1FZS350  iShares Developed Markets Property Yield UCITS ETF
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-106098910&menuId=1&pathName=ISHSII-DEV.MKT.PR.Y.DLDIS'
+  # 6 IE00B3VWM098  iShares MSCI USA Small Cap UCITS ETF B
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-20534544&menuId=1&pathName=ISHSVII-MSCI%20USA%20SC%20DL%20AC'
+  # 7 IE00B2QWDY88  iShares MSCI Japan Small Cap UCITS ETF (Acc) B
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-75100612&menuId=1&pathName=ISHSIII-MSCI%20J.SM.CAP%20DLD'
+  # 8 IE00B3VWMM18  iShares MSCI EMU Small Cap UCITS ETF B
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-20534543&menuId=1&pathName=ISHSVII-MSCI%20EMU%20SC%20EOACC'
+  # 9 DE000A0D8Q49  iShares DJ U.S. Select Dividend (DE)
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-3372758&menuId=1&pathName=IS.DJ%20U.S.SELEC.DIV.U.ETF'
+  #10 LU0292107645  db x-trackers MSCI EM TRN UCITS ETF 1C
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-10980122&menuId=1&pathName=DB%20X-TR.MSCI%20EM%20IDX.1C'
+  #11 IE00B0M63060  iShares UK Dividend UCITS ETF
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-105446142&menuId=1&pathName=ISHS-UK%20DIVIDEND%20LS%20D'
+  #12 DE000A0H0744  iShares DJ Asia Pacific Select Dividend 30
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-4076332&menuId=1&pathName=IS.DJ%20AS.PAC.S.D.30%20U.ETF'
+  #13 DE0002635281  iShares EURO STOXX Select Dividend 30 (DE)
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-2794238&menuId=1&pathName=IS.EO%20ST.SEL.DIV.30%20U.ETF'
+  #14 IE00BM67HM91  db x-tr.MSCI Wld.Energy I.ETF
+  urls << 'http://www.easycharts.at/index.asp?action=securities_securityDetails&id=tts-102824320&menuId=1&pathName=DB-XTR.MSCI%20WEIU(PD)%201CDL'
   
   picker = EtfQuoteFinanzNet.new
   urls.each do |url|
